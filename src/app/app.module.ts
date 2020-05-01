03bb79f5cacbd6c38a5bc0c3337648ec4ce134e7
Douglas Trigo <douglas.trigo@hotmail.com>
2020-05-01 09:03:55 -0300

    Adição do Toasty após excluir + mensagem de confirmação na pesquisa de lançamentos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
-import { CoreModule } from './core/core.module';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+import {ConfirmDialogModule} from 'primeng/confirmdialog';
+import {ConfirmationService} from 'primeng/api';
 
+import { CoreModule } from './core/core.module';
 import { PessoaService } from './pessoas/pessoa.service';
 import { LancamentoService } from './lancamentos/lancamento.service';
-
 import { AppComponent } from './app.component';
 import { PessoasModule } from './pessoas/pessoas.module';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
@@ -22,12 +25,17 @@ import { LancamentosModule } from './lancamentos/lancamentos.module';
     HttpClientModule,
     CoreModule,
 
+    ToastModule,
+    ConfirmDialogModule,
+
     LancamentosModule,
     PessoasModule
   ],
   providers: [
     LancamentoService,
-    PessoaService
+    PessoaService,
+    MessageService,
+    ConfirmationService
   ],
   bootstrap: [AppComponent]
 })
--- src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
+++ src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -2,6 +2,8 @@ import { LancamentoService, LancamentoFiltro } from './../lancamento.service';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { LazyLoadEvent } from 'primeng/api/public_api';
 import { Table } from 'primeng/table/table';
+import {MessageService} from 'primeng/api';
+import {ConfirmationService} from 'primeng/api';
 
 @Component({
   selector: 'app-lancamentos-pesquisa',
@@ -16,7 +18,9 @@ export class LancamentosPesquisaComponent implements OnInit {
   //Aqui estou referenciando a tabela do HTML através da marcação #tabela
   @ViewChild('tabela', {static: true}) grid: Table;
 
-  constructor(private lancamentoService: LancamentoService){ }
+  constructor(private lancamentoService: LancamentoService,
+              private messageService: MessageService,
+              private confirmation: ConfirmationService){ }
 
   ngOnInit(): void {
     /* this.pesquisar(); Não precisa mais desta pesquisa aqui,
@@ -38,13 +42,23 @@ export class LancamentosPesquisaComponent implements OnInit {
     this.pesquisar(pagina);
   }
 
+  confirmarExclusao(lancamento: any) {
+    this.confirmation.confirm({
+      message: 'Tem certeza que deseja excluir?',
+      accept: () => {
+        this.excluir(lancamento);
+      }
+    });
+  }
+
   excluir(lancamento: any){
     this.lancamentoService.excluir(lancamento.codigo)
-      .then(() => {
-        /*Neste ponto eu chamo o método reset(), ao fazer isso,
-        ele vai voltar para a primeira página e em seguida
-        disparar a pesquisa*/
-        this.grid.reset();
-      });
+    .then(() => {
+      /*Neste ponto eu chamo o método reset(), ao fazer isso,
+      ele vai voltar para a primeira página e em seguida
+      disparar a pesquisa*/
+      this.grid.reset();
+      this.messageService.add({severity:'success', detail:'Lançamento excluído com sucesso!'});
+    });
   }
 }
