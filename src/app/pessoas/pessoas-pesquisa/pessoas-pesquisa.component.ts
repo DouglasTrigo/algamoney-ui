f7fa788259d002c2cc255d218863b9a89c0fab7c
Douglas Trigo <douglas.trigo@hotmail.com>
2020-05-01 14:21:53 -0300

    Funcionalidade de excluir pessoa

--- src/app/pessoas/pessoas-pesquisa/pessoas-pesquisa.component.ts
+++ src/app/pessoas/pessoas-pesquisa/pessoas-pesquisa.component.ts
@@ -1,6 +1,11 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+
 import { LazyLoadEvent } from 'primeng/api/public_api';
+import { Table } from 'primeng/table/table';
+import { MessageService, ConfirmationService } from 'primeng/api';
+
+import { ErrorHandlerService } from './../../core/error-handler.service';
 import { PessoaService, PessoaFiltro } from './../pessoa.service';
-import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pessoas-pesquisa',
@@ -12,8 +17,12 @@ export class PessoasPesquisaComponent implements OnInit{
   totalRegistros = 0;
   filtro = new PessoaFiltro();
   pessoas = [];
+  @ViewChild('tabelaPessoas', { static: true}) grid: Table;
 
-  constructor(private pessoasService: PessoaService){}
+  constructor(private pessoasService: PessoaService,
+              private errorHandler: ErrorHandlerService,
+              private messageService: MessageService,
+              private confirmation: ConfirmationService){ }
 
   ngOnInit(): void {
     /* this.pesquisar(); Não precisa mais desta pesquisa aqui,
@@ -27,11 +36,30 @@ export class PessoasPesquisaComponent implements OnInit{
       .then(resultado => {
         this.totalRegistros = resultado.total;
         this.pessoas = resultado.pessoas;
-      });
+      })
+      .catch(erro => this.errorHandler.handle(erro));
   }
 
   aoMudarPagina(event: LazyLoadEvent){
     const pagina = event.first / event.rows;
     this.pesquisar(pagina);
+  }
+
+  confirmarExclusao(pessoa: any) {
+    this.confirmation.confirm({
+      message: 'Tem certeza que deseja excluir?',
+      accept: () => {
+        this.excluir(pessoa);
+      }
+    });
+  }
+
+  excluir(pessoa: any) {
+    this.pessoasService.excluir(pessoa.codigo)
+      .then(response => {
+        this.grid.reset();
+        this.messageService.add({severity:'success', detail:'Pessoa excluída com sucesso!'});
+      })
+      .catch(erro => this.errorHandler.handle(erro));
   }
 }
