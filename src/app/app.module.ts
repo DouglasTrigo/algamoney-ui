7ff0ebb71c066773b3f3a7b7298d97157d5273b9
Douglas Trigo <douglas.trigo@hotmail.com>
2020-05-01 09:25:27 -0300

    Configurando português na aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
 
 import { ToastModule } from 'primeng/toast';
 import { MessageService } from 'primeng/api';
@@ -14,6 +16,8 @@ import { LancamentoService } from './lancamentos/lancamento.service';
 import { AppComponent } from './app.component';
 import { PessoasModule } from './pessoas/pessoas.module';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
+
+registerLocaleData(localePt);
 
 @NgModule({
   declarations: [
@@ -35,7 +39,8 @@ import { LancamentosModule } from './lancamentos/lancamentos.module';
     LancamentoService,
     PessoaService,
     MessageService,
-    ConfirmationService
+    ConfirmationService,
+    { provide: LOCALE_ID, useValue: 'pt-BR'}
   ],
   bootstrap: [AppComponent]
 })
