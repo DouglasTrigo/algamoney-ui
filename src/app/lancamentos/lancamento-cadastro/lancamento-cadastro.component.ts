8e740b8884bfd8340da9224357298b1a060a2feb
Douglas Trigo <douglas.trigo@hotmail.com>
2020-05-01 16:30:54 -0300

    Populando combos do cadastro de lançamentos dinamicamente

--- src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
+++ src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -1,26 +1,53 @@
-import { Component } from '@angular/core';
+import { PessoaService } from './../../pessoas/pessoa.service';
+import { ErrorHandlerService } from './../../core/error-handler.service';
+import { CategoriaService } from './../../categorias/categoria.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-lancamento-cadastro',
   templateUrl: './lancamento-cadastro.component.html',
   styleUrls: ['./lancamento-cadastro.component.css']
 })
-export class LancamentoCadastroComponent {
+export class LancamentoCadastroComponent implements OnInit {
 
   tipos = [
     {label: 'Receita', value: 'RECEITA'},
     {label: 'Despesa', value: 'DESPESA'}
   ];
 
-  categorias = [
-    {label: 'Alimentação', value: 1},
-    {label: 'Transporte', value: 2}
-  ];
+  categorias = [];
 
-  pessoas = [
-    {label: 'João da Silva', value: 1},
-    {label: 'Sebastião Souza', value: 2},
-    {label: 'Maria Abadia', value: 3}
-  ];
+  pessoas = [];
+
+  constructor(
+    private categoriaService: CategoriaService,
+    private pessoaService: PessoaService,
+    private errorHandler: ErrorHandlerService
+  ) { }
+
+  ngOnInit(): void {
+    this.carregarCategorias();
+    this.carregarPessoas();
+  }
+
+  carregarCategorias() {
+    this.categoriaService.listarTodas()
+      .then(categorias => {
+        this.categorias = categorias.map(c => {
+          return { label: c.nome, value: c.codigo};
+        });
+      })
+      .catch(erro => this.errorHandler.handle(erro));
+  }
+
+  carregarPessoas() {
+    this.pessoaService.listarTodas()
+      .then(pessoas => {
+        this.pessoas = pessoas.map(p => {
+          return { label: p.nome, value: p.codigo };
+        });
+      })
+      .catch(erro => this.errorHandler.handle(erro));
+  }
 
 }
