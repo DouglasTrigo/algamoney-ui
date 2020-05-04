1cb4726ad0bccbd35f8fca7a6452f3b8c066d9d6
Douglas Trigo <douglas.trigo@hotmail.com>
2020-05-04 17:59:54 -0300

    Cadastro de lançamentos e pessoas funcionando

--- src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
+++ src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -1,7 +1,13 @@
+import { NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+
+import { MessageService } from 'primeng/api';
+
+import { Lancamento } from './../../core/model';
 import { PessoaService } from './../../pessoas/pessoa.service';
 import { ErrorHandlerService } from './../../core/error-handler.service';
 import { CategoriaService } from './../../categorias/categoria.service';
-import { Component, OnInit } from '@angular/core';
+import { LancamentoService } from './../lancamento.service';
 
 @Component({
   selector: 'app-lancamento-cadastro',
@@ -16,12 +22,14 @@ export class LancamentoCadastroComponent implements OnInit {
   ];
 
   categorias = [];
-
   pessoas = [];
+  lancamento = new Lancamento();
 
   constructor(
     private categoriaService: CategoriaService,
     private pessoaService: PessoaService,
+    private lancamentoService: LancamentoService,
+    private messageService: MessageService,
     private errorHandler: ErrorHandlerService
   ) { }
 
@@ -46,6 +54,17 @@ export class LancamentoCadastroComponent implements OnInit {
         this.pessoas = pessoas.map(p => {
           return { label: p.nome, value: p.codigo };
         });
+      })
+      .catch(erro => this.errorHandler.handle(erro));
+  }
+
+  salvar(form: NgForm) {
+    this.lancamentoService.adicionar(this.lancamento)
+      .then(() => {
+        this.messageService.add({severity: 'success', detail: 'Lançamento adicionado com sucesso!'});
+
+        form.reset();
+        this.lancamento = new Lancamento();
       })
       .catch(erro => this.errorHandler.handle(erro));
   }
