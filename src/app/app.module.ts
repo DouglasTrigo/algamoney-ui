e56597dfc7138c12fa34b3d70f5cfaca4eff01d0
Douglas Trigo <douglas.trigo@hotmail.com>
2020-05-07 15:49:56 -0300

    Módulo de segurança + tela de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { SegurancaModule } from './seguranca/seguranca.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -22,6 +23,7 @@ import { LancamentosModule } from './lancamentos/lancamentos.module';
 
     LancamentosModule,
     PessoasModule,
+    SegurancaModule,
     AppRoutingModule
   ],
   bootstrap: [AppComponent]
