3df7a2d0862a0f17b15aa33b9d180cd02fe93368
Douglas Trigo <douglas.trigo@hotmail.com>
2020-05-08 17:12:30 -0300

    Tratando acerto e erro na tela de login

--- src/app/seguranca/auth.service.ts
+++ src/app/seguranca/auth.service.ts
@@ -29,11 +29,15 @@ export class AuthService {
     return this.http.post(this.oauthTokenUrl, body, { headers })
       .toPromise()
       .then(response => {
-        console.log(response);
         this.amazenarToken(response['access_token']);
       })
       .catch(response => {
-        console.log(response);
+        const responseError = response.error;
+        if (response.status === 400
+            && responseError.error === 'invalid_grant') {
+              return Promise.reject('Usuário ou senha inválida');
+        }
+        return Promise.reject(response);
       });
   }
 
--- src/app/seguranca/login-form/login-form.component.ts
+++ src/app/seguranca/login-form/login-form.component.ts
@@ -1,5 +1,8 @@
-import { AuthService } from './../auth.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { ErrorHandlerService } from './../../core/error-handler.service';
+import { AuthService } from './../auth.service';
 
 @Component({
   selector: 'app-login-form',
@@ -8,15 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginFormComponent implements OnInit {
 
-  constructor(private auth: AuthService) { }
-  teste: AuthService;
+  constructor(
+    private auth: AuthService,
+    private errorHandler: ErrorHandlerService,
+    private router: Router
+  ) { }
 
-  ngOnInit(): void {
-    this.teste = this.auth;
-  }
+  ngOnInit(): void { }
 
   login(usuario: string, senha: string) {
-    this.auth.login(usuario, senha);
+    this.auth.login(usuario, senha)
+      .then(() => {
+        this.router.navigate(['/lancamentos']);
+      })
+      .catch(erro => this.errorHandler.handle(erro));
   }
 
 }
